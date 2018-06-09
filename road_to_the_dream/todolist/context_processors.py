2af2deeeb49c5166be440cc905100fb19dae22f2
Andrew <andrew.1.kulikov@gmail.com>
2018-06-10 00:26:28 +0300

    added context processor

--- road_to_the_dream/todolist/context_processors.py
+++ road_to_the_dream/todolist/context_processors.py
@@ -1,7 +1,9 @@
-from .models import TaskList
+from .models import TaskList, Tag
 
 
 def add_variable_to_context(request):
     return {
-        'task_lists': TaskList.objects.all()
+        'task_lists': TaskList.objects.all(),
+        'tags': Tag.objects.all(),
+        'user': request.user,
     }
