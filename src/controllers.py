190eb54bc3bfaa7c8e3ab72f8f83dbe7acbab076
Andrew <andrew.1.kulikov@gmail.com>
2018-06-26 15:55:05 +0300

    created controller interface

---
+++ src/controllers.py
@@ -0,0 +1,41 @@
+from .models import Task, TaskList
+
+
+class Controller:
+
+    def __init__(self):
+        pass
+
+    def add_task(self):
+        pass
+
+    def delete_task(self):
+        pass
+
+    def add_task_list(self):
+        pass
+
+    def delete_task_list(self):
+        pass
+
+    def get_sub_tasks(self):
+        pass
+
+    def get_all_tasks(self):
+        pass
+
+    def add_sub_task(self):
+        pass
+
+    def get_task(self):
+        pass
+
+    def edit_task(self):
+        pass
+
+    def edit_task_list(self):
+        pass
+
+    def invite_user(self):
+        pass
+
