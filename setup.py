597248388ae60404a2baf99e917cac79f862d922
Andrew <andrew.1.kulikov@gmail.com>
2018-06-27 19:34:57 +0300

    changed tests file name

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from os.path import join, dirname
 
 setup(
-    name='tasochki',
+    name='Road To The Dream',
     version='1.0',
     packages=find_packages(),
     long_description=open(join(dirname(__file__), 'README.md')).read(),
@@ -12,6 +12,7 @@ setup(
     entry_points={
        'console_scripts': [
            'rd = console_interface.console:main',
-           ]
-       }
+        ]
+    },
+    test_suite='tests.lib_tests'
 )
